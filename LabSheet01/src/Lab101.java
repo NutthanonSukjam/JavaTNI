0c9bfda42dc2a3b891997cdaa9b48a90cb82501e
NutthanonSukjam <cat.nutthanonsukjam@gmail.com>
2025-11-18 13:50:33 +0700

    NutthanonSukjamLabsheet01

---
+++ LabSheet01/src/Lab101.java
@@ -0,0 +1,8 @@
+public class Lab101 {
+    public static void main(String[] args) {
+        System.out.println("Hello, I am Nutthanon Sukjam");
+        //System.out.println("\"INT-108\" Object-Oriented Programming");
+        System.out.println("\tThis my first time learning Java");
+        System.out.println("\tI hope Get grade \"A\"");
+    }
+}
---
+++ LabSheet01/src/Lab105.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Lab105 {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Summation number = " + (2 + 5)+
+                "\nSummation number = " + (2-5)+
+                "\nSummation number = " + (2 * 5)+
+                "\nSummation number = " + (2 / 5.0)+
+                "\nSummation number = " + (2 % 5),
+                "Arithmetic Operators",
+                JOptionPane.WARNING_MESSAGE);
+    }
+}
